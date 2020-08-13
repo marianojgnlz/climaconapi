687657c6375c728cc0ea12dcb72bf8afa658d4f8
marianoGnlz <marianoGnlz@users.noreply.github.com>
2020-08-13 20:46:20 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,74 @@
+import React, { Fragment , useState, useEffect } from 'react';
+import Header from './components/Header';
+import Formulario from './components/Formulario';
+import Clima from './components/Clima';
+import Error from './components/Error'
+
+function App() {
+  const [busqueda, guardarBusqueda] = useState({
+      ciudad:'',
+      pais:''
+  });
+  const [consultar, guardarConsultar] = useState(false);
+  const [resultado, guardarResultado] = useState({});
+  const [error, guardarError] = useState(false);
+
+  const {ciudad, pais} = busqueda;
+
+  useEffect(() => {
+    const consultarAPI = async () => {
+      const apiId = '64ba0fb1f78feabca78155f769945c97';
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${apiId}`
+
+      const respuesta = await fetch(url);
+      const resultado = await respuesta.json();
+
+      if(resultado.cod === "404"){
+        guardarError(true);
+      } else {
+        guardarError(false);
+      }
+
+      guardarResultado(resultado);
+      guardarConsultar(false);
+  
+    }
+    if(consultar) consultarAPI();
+    // eslint-disable-next-line
+  }, [consultar]);
+
+  let componente;
+  if(error) {
+    componente = <Error mensaje="No hay resultados" />
+  } else {
+    componente = <Clima 
+                    resultado={resultado}
+                  />
+  }
+
+  return (
+    <Fragment>
+      <Header 
+        titulo='Tiempo React App'
+      />
+      <div className="contenedor-form">
+        <div className="container">
+          <div className="row">
+            <div className="col m6 s12">
+                <Formulario 
+                  busqueda={busqueda}
+                  guardarBusqueda={guardarBusqueda}
+                  guardarConsultar={guardarConsultar}
+                />
+            </div>
+            <div className="col m6 s12">
+              {componente}
+            </div>
+          </div>
+        </div>
+      </div>
+    </Fragment>
+  );
+}
+
+export default App;
